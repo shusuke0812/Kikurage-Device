365599a578f32b862c23d2d52dad96564fcb4fe6
shusuke0812 <shu.ota0812@gmail.com>
2023-01-21 15:05:58 +0900

    add: sample code

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include <Arduino.h>
+#include <M5Stack.h>
 
 void setup() {
   // put your setup code here, to run once:
+
+  M5.begin();
+
+  M5.Lcd.setCursor(20,60);
+  M5.Lcd.setTextSize(4);
+  M5.Lcd.print("Hello world");
 }
 
 void loop() {
