bcf4be9d487da54e025c15fd94e1548ee760aa04
shusuke0812 <shu.ota0812@gmail.com>
2023-01-22 15:55:37 +0900

    fix: to create sample code method

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,21 @@ void setup() {
 
   // initialize UART, Display, Power, microSD card
   M5.begin();
-
-  M5.Lcd.setCursor(20,60);
-  M5.Lcd.setTextSize(4);
-  M5.Lcd.setTextWrap(true);
-  M5.Lcd.print("Hello world");
-
-  M5.Lcd.fillRect(100, 100, 50, 60, TFT_GREEN);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+}
+
+// **** sample code ****
+
+void setupShowText() {
+  M5.Lcd.setCursor(20,60);
+  M5.Lcd.setTextSize(4);
+  M5.Lcd.setTextWrap(true);
+  M5.Lcd.print("Hello world");
+}
+
+void setupDrawRect() {
+  M5.Lcd.fillRect(100, 100, 50, 60, TFT_GREEN);
 }
