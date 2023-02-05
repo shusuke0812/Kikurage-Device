ee216b188ecd757faaab586053320fc0fc63356b
shusuke0812 <shu.ota0812@gmail.com>
2023-02-05 17:20:45 +0900

    add: Color class for converting to RGB

---
+++ src/sample/Color.cpp
@@ -0,0 +1,17 @@
+#include "Color.h"
+
+int Color::convertRGB(String s) {
+  int val = 0;
+  
+  for (int i = 0; i < 2; i++) {
+    val <<= 4;
+    char c = s.charAt(i);
+
+    if ((c >= '0') && (c <= '9')) {
+      val += c - '0';
+    } else if ((c >= 'a' && c <= 'f')) {
+      val += (c - 'a') + 10;
+    }
+  }
+  return val;
+}
---
+++ src/sample/Color.h
@@ -0,0 +1,11 @@
+#ifndef _Color_H_
+#define _Color_H_
+
+#include <M5Stack.h>
+
+class Color {
+  public:
+    int convertRGB(String s);
+};
+
+#endif // _Color_H_
