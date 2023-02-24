37ff08adb167bad277fb3e998010c7fa0de9bfce
shusuke0812 <shu.ota0812@gmail.com>
2023-02-24 09:51:06 +0900

    fix: comments

--- src/bluetooth/KikurageBLEServer.cpp
+++ src/bluetooth/KikurageBLEServer.cpp
@@ -6,14 +6,13 @@ KikurageBLEUUID id;
 void KikurageBLEServer::initialize() {
   NimBLEDevice::init("kikurage-device-m5-stack");
 
-  // Callbacks for connected and disconnected
+  // Service
   NimBLEServer *peripheralServer = NimBLEDevice::createServer();
   peripheralServer->setCallbacks(new KikurageBLEServerCallbacks());
 
-  // Create service
   NimBLEService *peripheralService = peripheralServer->createService(id.SERVICE_UUID);
 
-  // Create characteristics
+  // Characteristic
   peripheralCharacteristic[0] = peripheralService->createCharacteristic(
     id.CHARACTERISTICS[0],
     NIMBLE_PROPERTY::WRITE
