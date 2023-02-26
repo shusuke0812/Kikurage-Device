4328d34868fa694807231a8fa88152130141ea38
shusuke0812 <shu.ota0812@gmail.com>
2023-02-26 12:01:42 +0900

    fix: callback members

--- src/bluetooth/KikurageBLEServerCallbacks.h
+++ src/bluetooth/KikurageBLEServerCallbacks.h
@@ -1,15 +1,12 @@
 #ifndef _KikurageBLEServerCallbacks_H_
 #define _KikurageBLEServerCallbacks_H_
 
-#include <NimBLEServer.h>
 #include <NimBLEDevice.h>
 #include <M5Stack.h>
 
 class KikurageBLEServerCallbacks: public NimBLEServerCallbacks {
     public:
     bool isConnected;
-    //virtual void onConnect(NimBLEServer *peripheralServer);
-    //virtual void onDisconnect(NimBLEServer *peripheralServer);
 };
 
 #endif // _KikurageBLEServerCallbacks_H
