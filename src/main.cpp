32fa679f61fe3acb61130d31a122633fc1c0861c
shusuke0812 <shu.ota0812@gmail.com>
2023-03-14 10:15:42 +0900

    fix: main loop as running setupWiFiToPeripheral()

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <Arduino.h>
 #include <M5Stack.h>
+
 #include "bluetooth/KikurageBLEServer.h"
 #include "bluetooth/KikurageBLEMessage.h"
 #include "bluetooth/KikurageBLEState.h"
 
+#include "wifi/WiFiScanner.h"
+#include "wifi/KikurageWiFi.h"
+
 KikurageBLEServer kBLEServer;
+WiFiScanner wifiScanner;
 
 // put your setup code here, to run once
 void setup() {
@@ -19,8 +24,15 @@ void setup() {
 
 // put your main code here, to run repeatedly
 void loop() {
+    if (isBLEConnected) {
+        std::vector<KikurageWiFi> wifiList = wifiScanner.getWiFiList();
+        int wifiListCount = wifiList.size();
+
+        for (int i = 0; i < wifiListCount; i++) {
+            String jsonString = getKikurageWiFiJSONString(wifiList[i]);
+            kBLEServer.setupWiFiToPeripheral(jsonString);
+        }
+    }
     delay(1000);
     M5.update();
-    //sample.loopVolume();
-    //sample.loopMPU9250();
 }
